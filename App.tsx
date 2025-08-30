fca8d351d6a00e0b63236c2c3e93481900c0c8e8
stmorgan.eth <127997825+StanleyMorgan@users.noreply.github.com>
2025-08-30 13:06:19 +0300

    feat: Use CDN for icons
    Updates the application to use icons hosted on a GitHub CDN. This ensures consistent loading and reduces the need to bundle static assets.

--- App.tsx
+++ App.tsx
@@ -1,6 +1,3 @@
-
-
-
 import React, { useState, useEffect, useCallback } from 'react';
 import { Header } from './components/Header';
 import { MainView } from './components/MainView';
@@ -26,7 +23,7 @@ const config = getDefaultConfig({
   appName: 'Chainsaw',
   appDescription: 'A web application to interact with smart contracts on different chains via a user-configured interface. Connect your wallet, configure your buttons, and execute transactions with ease.',
   appUrl: window.location.origin,
-  appIcon: '/icons/icon128.png',
+  appIcon: 'https://raw.githubusercontent.com/StanleyMorgan/Chainsaw-config/main/icons/icon128.png',
   projectId: projectId,
   chains: [mainnet, optimism, base, polygon, arbitrum],
   ssr: false, // If your dApp uses server side rendering (SSR)
